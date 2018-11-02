a2daee3829dabca85f06753166e35f807e8288fa
Noah Burwell <noahb343@gmail.com>
2018-11-01 17:10:12 -0700

    Class cards done

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'TofuTexts' });
+  res.render('index', { page_title: 'Home' });
 });
 
 module.exports = router;
